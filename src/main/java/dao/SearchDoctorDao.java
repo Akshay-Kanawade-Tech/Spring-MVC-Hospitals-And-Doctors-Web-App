3c0c85492dc96c21b919b42e263058cd09e11ee4
Akshay-Mahagan <tech.akshayk@gmail.com>
2023-09-28 11:19:37 +0530

    This is simple MVC Application

---
+++ src/main/java/dao/SearchDoctorDao.java
@@ -0,0 +1,36 @@
+package dao;
+
+import domain.SearchDoctor;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import java.sql.SQLException;
+
+public class SearchDoctorDao {
+
+    JdbcTemplate jdbcTemplate;
+
+    public void setJdbcTemplate(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate= jdbcTemplate;
+    }
+
+    public SearchDoctor getDoctorByName(String doctorName) throws SQLException{
+        String sql ="select *from DoctorsDetails where doctorName= ?";
+        return jdbcTemplate.queryForObject(sql , new Object[]{doctorName}, new BeanPropertyRowMapper<SearchDoctor>(SearchDoctor.class));
+    }
+    public SearchDoctor getDoctorByRegistrationNumber(String doctorRegistrationNumber) throws SQLException{
+        String sql ="select *from DoctorsDetails where doctorRegistrationNumber= ?";
+        return jdbcTemplate.queryForObject(sql , new Object[]{doctorRegistrationNumber}, new BeanPropertyRowMapper<SearchDoctor>(SearchDoctor.class));
+    }
+
+    public SearchDoctor getDoctorByCity(String city) throws SQLException{
+        String sql ="select *from DoctorsDetails where city= ?";
+        return jdbcTemplate.queryForObject(sql , new Object[]{city}, new BeanPropertyRowMapper<SearchDoctor>(SearchDoctor.class));
+    }
+
+    public SearchDoctor getDoctorByGender(String gender) throws SQLException{
+        String sql ="select *from DoctorsDetails where gender= ?";
+        return jdbcTemplate.queryForObject(sql , new Object[]{gender}, new BeanPropertyRowMapper<SearchDoctor>(SearchDoctor.class));
+    }
+
+}
